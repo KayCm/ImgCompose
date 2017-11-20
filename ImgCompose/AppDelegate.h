c917a70150f0a661467bff2de51040a484dbe935
KayCM <kaycm@KayCMdeiMac.local>
2017-11-20 10:31:43 +0800

    Initial Commit

---
+++ ImgCompose/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImgCompose
+//
+//  Created by KayCM on 2017/11/20.
+//  Copyright © 2017年 KayCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImgCompose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImgCompose
+//
+//  Created by KayCM on 2017/11/20.
+//  Copyright © 2017年 KayCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
